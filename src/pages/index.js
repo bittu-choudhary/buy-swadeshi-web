cde280751eaf61bcf216e3f403c837a9cf87958e
bittu-choudhary <bittuchoudhary007@gmail.com>
2020-06-01 17:27:59 +0530

    change header styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Search from "../components/SearchContainer"
+import styles from '../components/search-container-css-modules.module.css'
+
 
 const IndexPage = () => (
   <div>
-    <h1 style={{ marginTop: `3em`, textAlign: `center` }}>
+    <h1 style={{ marginTop: `1em`, marginBottom: `4em`, textAlign: `center` }}>
       Buy Swadeshi
     </h1>
-    <div>
+    <div className={styles.searchContainer}>
       <Search />
     </div>
   </div>
