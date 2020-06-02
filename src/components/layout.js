2207f698d5837c704acb583a5e1a0bcac086de66
bittu-choudhary <bittuchoudhary007@gmail.com>
2020-06-02 17:46:45 +0530

    change website title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import { BsHeartFill } from "react-icons/bs"
 import IndiaFLagImg from '../images/india-flag-16.png'
 
@@ -10,7 +9,7 @@ export default function Layout({ children }) {
         <div style={ { margin: 'auto', width: 'fit-content' }}>
           <div>
             <h1 style={{ marginTop: `0px`, marginBottom: `0px`, textAlign: `center` }}>
-              Buy Swadeshi
+              Find Swadeshi
             </h1>
           </div>
           <div>
@@ -21,7 +20,7 @@ export default function Layout({ children }) {
       {children}
       <footer style={{ backgroundColor: `rgb(255, 153, 51)`, backgroundSize: `contain` }}>
         <div style={ { width: 'fit-content', margin: 'auto' }}>
-          <p style={ {  }}>Made with <BsHeartFill style={{color: 'red', marginBottom: '-3px'}}/> by <img src={IndiaFLagImg}></img> for <img src={IndiaFLagImg}></img></p>
+          <p style={ {  }}>Made with <BsHeartFill style={{color: 'red', marginBottom: '-3px'}}/> by <img src={IndiaFLagImg} alt=""></img> for <img alt="" src={IndiaFLagImg}></img></p>
         </div>
       </footer>
     </div>
