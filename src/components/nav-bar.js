e9d049ba3b3a428d2cf87281f87c08d6454c1dc5
bittu-choudhary <bittuchoudhary007@gmail.com>
2020-06-03 15:34:37 +0530

    add google play badge in nav bar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -3,21 +3,30 @@ import { useTranslation } from "react-i18next"
 import styles from './search-container-css-modules.module.css'
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GooglePlayBadgeEn from '../images/google-play-badge.png'
+import GooglePlayBadgeHi from '../images/google-play-badge-hi.png'
 import { Link } from "gatsby"
+import Image from  'react-bootstrap/Image'
 
 const NavBar = ({props}) => {
   const { t, i18n } = useTranslation()
+  let GooglePlayBadge = GooglePlayBadgeEn
+  if (i18n.language === "hi") {
+    GooglePlayBadge = GooglePlayBadgeHi
+  }
   return (
     <div className={styles.navBarHeader} style={{
       width: `fit-content`,
-      float: `right`,
       marginRight: `80px`,
-      marginTop: `-30px`,
-      display: `none`
+      marginTop: `-90px`,
+      display: `none`,
+      padding: `24px`
     }}>
-      <Button href="https://forms.gle/fB2VUuEHCfpadnrv8" target="_blank" variant="info" style={{marginRight: `2px`, padding: `.075rem .375rem`}}>Feedback</Button>
-      <Button variant="info" style={{marginRight: `2px`, padding: `.075rem .375rem`}}>Download App</Button>
-      <Button href="whatsapp://send?text=The text to share!" data-action="share/whatsapp/share" variant="info" style={{padding: `.075rem .375rem`}}>Share</Button>
+      <Button href="https://forms.gle/fB2VUuEHCfpadnrv8" target="_blank" variant="info" style={{marginRight: `2px`, padding: `.075rem .375rem`}}>{t('feedback')}</Button>
+      <a  href="https://www.google.com" target="_blank"  >
+      <Image style={{ marginRight: `2px`, padding: `.075rem .375rem`}} src={GooglePlayBadge} style={{width: `112px`}} alt="">
+      </Image></a>
+      <Button href="whatsapp://send?text=The text to share!" data-action="share/whatsapp/share" variant="info" style={{padding: `.075rem .375rem`}}>{t('share')}</Button>
     </div>
   )
 }
