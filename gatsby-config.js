0117ac7a3508be7992c897197fb20de416d92746
bittu-choudhary <bittuchoudhary007@gmail.com>
2020-06-08 20:06:53 +0530

    add dotenv config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
 
 module.exports = {
   /* Your site config here */
