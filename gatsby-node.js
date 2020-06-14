aaf15d68ed5338dceb25847d40e7638ed32a60ab
bittu-choudhary <bittuchoudhary007@gmail.com>
2020-06-15 03:51:45 +0530

    modify indexing script acc to new json str

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,15 +2,36 @@
 const write = require("write");
 const path = require("path");
 const Bm25 = require( "./src/library/wink-bm25-text-search")
-const JSONData = require("./content/brand_list.json")
+const JSONData = require("./content/new_brand_list.json")
 
+var _ = require('lodash') 
 const brandList = JSONData.brands
 const engine = Bm25()
-engine.defineConfig( { fldWeights: { name: 1, category: 2 } } )
-brandList.map( ( doc, i ) => {
-  // Note, 'i' becomes the unique id for 'doc'
-  engine.addDoc( doc, i )
-} )
+engine.defineConfig( { fldWeights: { name: 1} } )
+for (var key in JSONData) {
+  if (key === "version") {
+    continue
+  }
+  for (const dataPoints in JSONData[key]) {
+    var dataPointType;
+    switch (key) {
+      case "categories":
+        dataPointType = "_typeCategory"
+        break;
+      case "companies":
+        dataPointType = "_typeCompany"
+        break;
+      case "products":
+        dataPointType = "_typeProduct"
+        break;
+      default:
+        break;
+    }
+    var indexId =JSONData[key][dataPoints]["id"] + dataPointType
+    doc = _.pick(JSONData[key][dataPoints], ['name']) // extract name from object
+    engine.addDoc( doc, indexId )
+  }
+}
 engine.consolidate()
 const consolidatedJson =  engine.exportJSON()
-write.sync("./content/indexed_data.json", JSON.stringify(consolidatedJson))
+write.sync("./content/new_indexed_data.json", JSON.stringify(consolidatedJson))
