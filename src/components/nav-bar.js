b6a70b64f5e9789c696df0522adf598e72303d39
bittu-choudhary <bittuchoudhary007@gmail.com>
2020-06-17 17:34:36 +0530

    update nav bar component to handle body bar as well

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -17,23 +17,39 @@ const sendFirebaseAnalytics = (event) => {
   }
 }
 
-
-const NavBar = ({props}) => {
+const NavMenu = (props) => {
   const { t, i18n } = useTranslation()
   let GooglePlayBadge = GooglePlayBadgeEn
   if (i18n.language === "hi") {
     GooglePlayBadge = GooglePlayBadgeHi
   }
-
   return (
-    <div className={styles.navBarHeader}>
-      <Button onClick={() => sendFirebaseAnalytics("feedback_click")} rel="noreferrer" className={styles.navBarButton} href="https://forms.gle/fB2VUuEHCfpadnrv8" target="_blank" variant="info" style={{ width: `85px`, marginRight: `2px`, padding: `.075rem .375rem`}}>{t('feedback')}</Button>
-      <a onClick={() => sendFirebaseAnalytics("play_button_click")}  title="Download our Android app" rel="noreferrer" href="https://play.google.com/store/apps/details?id=store.buyswadeshi.android" target="_blank"  >
-      <Image style={{ marginRight: `2px`, padding: `.075rem .375rem`}} src={GooglePlayBadge} style={{width: `112px`}} alt="Download our Android App">
-      </Image></a>
-      <Button onClick={() => sendFirebaseAnalytics("web_share")} className={styles.navBarButton} href={"whatsapp://send?text=" + t('share_text')} data-action="share/whatsapp/share" variant="info" style={{width: `85px`, padding: `.075rem .375rem`}}>{t('share')}</Button>
-    </div>
+    <>
+      <Button  onClick={() => sendFirebaseAnalytics("feedback_click")} rel="noreferrer" className={styles.navButton + " " + styles.feedbackButton} href="https://forms.gle/fB2VUuEHCfpadnrv8" target="_blank" variant="info">{t('feedback')}</Button>
+        <a onClick={() => sendFirebaseAnalytics("play_button_click")} title="Download our Android app"  rel="noreferrer" href="https://play.google.com/store/apps/details?id=store.buyswadeshi.android" target="_blank"  >
+        <Image className={styles.navAppButton + " " + styles.playStoreButton} src={GooglePlayBadge} alt="Download our Android App">
+        </Image></a>      
+        <Button onClick={() => sendFirebaseAnalytics("web_share")} className={styles.navButton} href={"whatsapp://send?text=" + t('share_text')} data-action="share/whatsapp/share" variant="info" style={{ width: `85px`, padding: `.075rem .375rem`}}>{t('share')}</Button>
+    </>
   )
+}
+
+
+const NavBar = (props) => {
+  const {fromHeader} = props
+  if (fromHeader) {
+    return(
+        <div className={styles.navBarHeader}>
+          <NavMenu/>
+        </div>
+      )
+  } else {
+    return(
+      <div className={styles.navBarBody}>
+        <NavMenu/>
+      </div>
+      )
+  }
 }
 
 export default NavBar
