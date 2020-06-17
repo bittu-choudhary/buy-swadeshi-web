270649e3b55efabae77d4f87a01107a47dcd0aef
bittu-choudhary <bittuchoudhary007@gmail.com>
2020-06-18 00:18:59 +0530

    add nav bar and home screen message component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React, {Component} from "react"
 import { withTrans } from '../i18n/withTrans'
 import Footer from './footer'
 import Header from './header'
+import Search from "../components/SearchContainer"
+import styles from '../components/search-container-css-modules.module.css'
+import SEO from "../components/seo"
+import NavBar from '../components/nav-bar'
+import HomePageMessage from '../components/home-page-message'
 
 // Menu = (props) => {
 //   return(
@@ -16,14 +21,36 @@ import Header from './header'
 // }
 
 
-const Layout =  ( {children}) => {
-  return (
-    <div>
-      <Header/>
-      {children}
-      <Footer/>
-    </div>
-  )
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      showMessage: this.props.showMessage
+     };
+  }
+
+  toggleMessage = (newVal) => {
+    this.setState({showMessage: newVal})
+  }
+
+  render() {
+    const {toggleMessage} = this.props
+    return (
+      <div>
+        <Header/>
+        <SEO/>
+        <div style={{marginBottom: '6rem'}}>
+          <div className={styles.searchContainer}>
+            <NavBar fromHeader={false}/>
+            <HomePageMessage showAlert={this.state.showMessage}/>
+            <Search toggleMessage={this.toggleMessage} toggleCategoryView={this.props.toggleCategoryView}/>
+            {this.props.children}
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
 }
 
 export default withTrans(Layout)
