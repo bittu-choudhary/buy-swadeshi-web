02f2d84665af28cdd55dddf9dd5f16d55b71e58a
bittu-choudhary <bittuchoudhary007@gmail.com>
2020-06-26 21:07:55 +0530

    add utils

---
+++ src/utils/typography.js
@@ -0,0 +1,40 @@
+import Typography from "typography"
+
+const options = {
+  googleFonts: [
+    {
+      name: `Noto Sans JP`,
+      styles: [`400`, `400i`, `700`, `700i`],
+    },
+  ],
+  baseLineHeight: 1.4,
+  headerColor: `hsla(0,0%,0%,0.8)`,
+  bodyColor: `hsla(0,0%,0%,0.7)`,
+  blockMarginBottom: 0.75,
+  headerFontFamily: [`Noto Sans JP`],
+  bodyFontFamily: [`Noto Sans JP`],
+  overrideStyles: () => {
+    return {
+      html: {
+        overflowY: `scroll`,
+      },
+      h1: {
+        lineHeight: 1.1,
+      },
+      a: {
+        color: `#3897f0`,
+      },
+    }
+  },
+}
+
+const typography = new Typography(options)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles()
+}
+
+export default typography
+export const scale = typography.scale
+export const rhythm = typography.rhythm
